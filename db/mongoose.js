7ee01e03df3faef6e752d0084dcc81f4631b0803
Tad <ntdat369842@gmail.com>
2019-02-05 11:26:59 +0700

    testing deploy-4

--- db/mongoose.js
+++ db/mongoose.js
@@ -3,6 +3,11 @@ let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.set('useCreateIndex', true);
 mongoose.connect(process.env.MONGOLAB_URI, { useNewUrlParser: true });
+mongoose.connection.once('open', function(){
+    console.log('Conection has been made!');
+}).on('error', function(error){
+    console.log('Error is: ', error);
+});
 
 module.exports = {
     mongoose
